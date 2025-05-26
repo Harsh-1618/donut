4855c0144369d18e364ad36abd8d64082f0b38c8
Harsh Sharma <harshdsharma08@gmail.com>
2025-05-26 20:26:07 +0530

    Feat: Add all 9 subcubes Although it works, it's rendering all the points, even the ones which are on the back and should be impossible to see on a 2D screen

--- cube_solid_slow.py
+++ cube_solid_slow.py
@@ -4,74 +4,88 @@ import time
 
 screen_height = 600
 screen_width = 1200
-linear_spacing = 3
+linear_spacing = 1 # spacing between points in pixels
+sub_cube_spacing = 2 #1
 pixel_scaling = 20 # one unit is equal to 20 pixels in my unit
-r1_unit = 1 # radius of the circle in my own pixel scale units
-r2_unit = 0
-k2_unit = 5 #10
+r1_unit = 1 # half side length of cube
+k2_unit = 20 # 12
 A = 0
 B = 0
 
-r1_pixel = r1_unit * pixel_scaling
-r2_pixel = r2_unit * pixel_scaling
-k2_pixel = k2_unit * pixel_scaling
+r1_pixel = int(r1_unit * pixel_scaling)
+k2_pixel = int(k2_unit * pixel_scaling)
+sub_cube_spacing_pixel = int(sub_cube_spacing * pixel_scaling)
 
-k1_pixel = int((screen_height*k2_pixel*3)/(8*(r1_pixel+r2_pixel+120)))
+k1_pixel = int((screen_height*k2_pixel*3)/(8*(r1_pixel+180))) # 120 instead of 180
 
 screen = np.zeros((screen_height, screen_width, 3), dtype=np.uint8)
 x_displacement = screen.shape[1]//2
 y_displacement = screen.shape[0]//2
 
-# making square
-points = np.arange(r2_pixel-r1_pixel, r2_pixel+r1_pixel, linear_spacing)
-
+# Making filled square
+points = np.arange(-r1_pixel, r1_pixel, linear_spacing)
 xx, yy = np.meshgrid(points, points)
 x_points = xx.flatten()
 y_points = yy.flatten()
 
-# making cube
-cube_x = []
-cube_y = []
-cube_z = []
-for point in points:
-    cube_x.extend(x_points)
-    cube_y.extend(y_points)
-    cube_z.extend([point]*len(x_points))
+# Making filled cube's core
+center_cube_x = np.repeat(x_points[np.newaxis, :], len(points), axis=0).flatten()
+center_cube_y = np.repeat(y_points[np.newaxis, :], len(points), axis=0).flatten()
+center_cube_z = np.repeat(points, len(x_points)) # repeat Repeats each element of an array after themselves
 
-extended_y = []
-for y_val in cube_y:
-    extended_y.append(y_val+60)
+# Making full filled cube
+cube_dict = {} # keys -> xyz position
+sub_cubes = ["-1_-1_-1","0_-1_-1","1_-1_-1","-1_0_-1","0_0_-1","1_0_-1","-1_1_-1","0_1_-1","1_1_-1",
+            "-1_-1_0","0_-1_0","1_-1_0","-1_0_0", "0_0_0", "1_0_0","-1_1_0","0_1_0","1_1_0",
+            "-1_-1_1","0_-1_1","1_-1_1","-1_0_1","0_0_1","1_0_1","-1_1_1","0_1_1","1_1_1"
+]
 
-cube_x.extend(cube_x)
-cube_y.extend(extended_y)
-cube_z.extend(cube_z)
+for cube in sub_cubes:
+    x_shift, y_shift, z_shift = [int(c) for c in cube.split("_")]
+    x_shift_pixel = x_shift*(sub_cube_spacing_pixel + 2*r1_pixel)
+    y_shift_pixel = y_shift*(sub_cube_spacing_pixel + 2*r1_pixel)
+    z_shift_pixel = z_shift*(sub_cube_spacing_pixel + 2*r1_pixel)
+
+    cube_dict[cube] = (
+        center_cube_x + x_shift_pixel,
+        center_cube_y + y_shift_pixel,
+        center_cube_z + z_shift_pixel,
+    )
 
 while cv2.waitKey(1) != ord('q'):
     screen[:, :] = 0
-    screen[y_displacement, :] = 255
-    screen[:, x_displacement] = 255
+    screen[y_displacement, :, :] = 255
+    screen[:, x_displacement, :] = 255
 
-    cube_x_proj = []
-    cube_y_proj = []
-    for i in range(len(cube_x)):
-        # rotation along x axis:
-        x_rotate1 = cube_x[i]
-        y_rotate1 = cube_y[i]*np.cos(A) - cube_z[i]*np.sin(A)
-        z_rotate1 = cube_y[i]*np.sin(A) + cube_z[i]*np.cos(A)
+    cos_a = np.cos(A)
+    sin_a = np.sin(A)
+    cos_b = np.cos(B)
+    sin_b = np.sin(B)
 
-        # rotation along z axis:
-        x_rotate2 = x_rotate1*np.cos(B) - y_rotate1*np.sin(B)
-        y_rotate2 = x_rotate1*np.sin(B) + y_rotate1*np.cos(B)
+    for k, cube in cube_dict.items():
+        x_shift, y_shift, z_shift = [int(c) for c in k.split("_")]
+        cube_x = cube[0]
+        cube_y = cube[1]
+        cube_z = cube[2]
 
-        cube_x_proj.append(int((k1_pixel*x_rotate2)/(k2_pixel+z_rotate1)) + x_displacement)
-        cube_y_proj.append(int((k1_pixel*y_rotate2)/(k2_pixel+z_rotate1)) + y_displacement)
+        # rotation along x axis
+        x_rotate1 = cube_x
+        y_rotate1 = cube_y*cos_a - cube_z*sin_a
+        z_rotate1 = cube_y*sin_a + cube_z*cos_a
 
-    y_proj_plot = screen_height - 1 - np.array(cube_y_proj)
-    screen[y_proj_plot, cube_x_proj, 0] = 0
-    screen[y_proj_plot, cube_x_proj, 1] = 255
-    screen[y_proj_plot, cube_x_proj, 2] = 255
+        # rotation along z axis
+        x_rotate2 = x_rotate1*cos_b - y_rotate1*sin_b
+        y_rotate2 = x_rotate1*sin_b + y_rotate1*cos_b
 
-    cv2.imshow("window", screen)
+        cube_x_proj = ((k1_pixel*x_rotate2)/(k2_pixel+z_rotate1)).astype(np.int16) + x_displacement
+        cube_y_proj = ((k1_pixel*y_rotate2)/(k2_pixel+z_rotate1)).astype(np.int16) + y_displacement
+
+        y_proj_plot = screen_height - 1 - cube_y_proj
+        screen[y_proj_plot, cube_x_proj, :] = [(x_shift+2) * 80, (y_shift+2) * 80, (z_shift+2) * 80]
+
+    cv2.imshow("window", screen) # need to implement depth filtering. and illumination ofcourse
 
     A+=0.04
     B+=0.02
+
+    # time.sleep(5e-2)
